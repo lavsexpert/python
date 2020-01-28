ab9fe16e53271e3cd6d517078a8cf5b7159d15aa
lavsexpert <lavrov-sergey@yandex.ru>
2020-01-28 19:19:35 +0300

    Сделана задача 1 в 14 занятии

---
+++ 14/1.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+file = open('bank.db', 'w')
+file.close()
+
+conn = sqlite3.connect('bank.db')
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE cur
+(name CHAR(50), kurs FLOAT, multy FLOAT)
+""")
+
+cursor.execute("""
+    INSERT INTO cur
+    VALUES(?,?,?)""",("Доллар", 62.3, 1.0))
+
+cursor.execute("""
+    INSERT INTO cur
+    VALUES(?,?,?)""",("Доллар", 68.7, 1.0))
+
+cursor.execute("""
+    INSERT INTO cur
+    VALUES(?,?,?)""",("Фунт", 96.3, 1.0))
+
+cursor.execute("""
+    INSERT INTO cur
+    VALUES(?,?,?)""",("Юань", 10.1, 1.0))
+
+cursor.execute("""
+    INSERT INTO cur
+    VALUES(?,?,?)""",("Йена", 60.5, 100.0))
+
+cursor.execute("SELECT * FROM cur")
+cur = cursor.fetchall()
+for item in cur:
+    print(item[0], "-", item[1], "-", item[2])
+conn.close()
