f9989ec0bd2f1baa438b07453ee91e14ab031082
Sergey Lavrov <lavrov-sergey@yandex.ru>
2020-02-11 22:06:57 +0300

    Создание сервера для 18го урока

---
+++ 18/server/requests.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        path = self.path
+        if path == "/":
+            path = "/index.html"
+        if path.split("/")[1] == "img":
+            self.set_headers('image/*,image/jpeg,image/png')
+            file = open("../client" + path, 'rb')
+            self.get_content(file, file.read())
+        else:
+            self.set_headers('text/html')
+            try:
+                file = open("../client" + path.split("?")[0], "r", encoding='utf-8')
+            except FileNotFoundError:
+                file = open("../client/404.html", "r", encoding='utf-8')
+            self.get_content(file, bytes(file.read(), "utf-8"))
+        return
+
+    def set_headers(self, content_type):
+        self.send_response(200)
+        self.send_header('Content-type', content_type)
+        self.end_headers()
+        return
+
+    def get_content(self, file, content):
+        self.wfile.write(content)
+        file.close()
+        return
